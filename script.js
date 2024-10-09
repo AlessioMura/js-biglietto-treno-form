5294c69fe1509d07d61befa8372fb2b4776c66e9
AlessioMura <alessiomura03@gmail.com>
2024-10-09 17:07:32 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//milestone 1
+//prep
+
+const age_field = document.getElementById('age');
+const km_field = document.getElementById('km');
+const btn = document.getElementById('confirm_btn');
+const price_for_km = 0.21;
+const disc_underage = 0.2;
+const disc_over65 = 0.4;
+
+//process
+
