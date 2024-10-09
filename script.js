7a7b6a80cd01134daf13b215ff9ff88a209075ef
AlessioMura <alessiomura03@gmail.com>
2024-10-09 18:00:00 +0200

    adjusted script partially to html

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //milestone 1
 //prep
 
-const age_field = document.getElementById('age');
-const km_field = document.getElementById('km');
-const btn = document.getElementById('confirm_btn');
+let age_field = document.getElementById('age_select');
+let km_field = document.getElementById('km');
+let btn = document.getElementById('confirm_btn');
 const price_for_km = 0.21;
 const disc_underage = 0.2;
 const disc_over65 = 0.4;
@@ -14,16 +14,16 @@ btn.addEventListener('click', calc_price => {
 
     let age = age_field.value;
     let km = km_field.value;
+    let full_ticket_price = (km * price_for_km);
+    let price_disc_under = full_ticket_price - (full_ticket_price * disc_underage);
+    let price_disc_over = full_ticket_price - (full_ticket_price *disc_over65);
 
-    let total_price = (km * price_for_km);
 
-    if (age < 18) {
-        total_price = total_price - (total_price * disc_underage);
-        console.log(total_price.toFixed(2) + "€");
-    } else if (age >= 65) {
-        total_price = total_price - (total_price *disc_over65);
-        console.log(total_price.toFixed(2) + "€");
+    if (age === '1') {
+        console.log(price_disc_under.toFixed(2) + "€");
+    } else if (age === '3') {
+        console.log(price_disc_over.toFixed(2) + "€");
     } else {
-        console.log(total_price.toFixed(2) + "€");
+        console.log(full_ticket_price.toFixed(2) + "€");
     }
 })
